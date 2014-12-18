3eb702a3c42400e1c530a104ea27f48acd2241f0
Stuart Young <stuart@cyphergenomics.com>
2014-12-18 03:32:34 -0800

    -Fix lib/FilterReads: Refactored isPairedBam method to use 'samtools flagstats'

--- lib/FilterReads.pm
+++ lib/FilterReads.pm
@@ -1,4 +1,6 @@
 use MooseX::Declare;
+use Method::Signatures::Simple;
+
 
 use strict;
 use warnings;
@@ -9,14 +11,11 @@ class FilterReads extends Common {
 
 use FindBin qw($Bin);
 
-use Agua::CLI::App;
-use Conf::Yaml;
-
 # Integers
 has 'sleep'			=> 	( isa => 'Str|Undef', is => 'rw', default	=>	10 );
 
 # Strings
-has 'keyfile'		=> 	( isa => 'Str|Undef', is => 'rw', required	=>	1 );
+#has 'keyfile'		=> 	( isa => 'Str|Undef', is => 'rw', required	=>	1 );
 has 'uuid'			=> 	( isa => 'Str|Undef', is => 'rw', required 	=> 	0 );
 has 'version'		=> 	( isa => 'Str|Undef', is => 'rw', default	=>	"" );
 has 'options'		=> 	( isa => 'Str|Undef', is => 'rw', default	=>	"-l200m" );
@@ -29,87 +28,58 @@ method filterReads ($uuid, $inputfile, $outputfile) {
 	$self->logDebug("inputfile", $inputfile);
 	$self->logDebug("outputfile", $outputfile);
 	
-	#my $projects	=	[ "broadbp" ];
-	#my $installdir	=	$self->getInstallDir($project);
-	#$self->logDebug("installdir", $installdir);
-	#my $packagename	=	uc($project);
-	
-	my $validated	=	$self->bamIsAPair($inputfile);
+	my $validated	=	$self->isPairedBam($inputfile);
 	$self->logDebug("validated",$validated);
+	
+	
+$self->logDebug("DEBUG EXIT") and exit;
+
+
 	if ( $validated ) {
 			
 			my $bam_installdir	=	$self->getInstallDir("bamtools");
-	        my $bamtools	=	"$bam_installdir/bamtools";
+	        my $bamtools	=	"$bam_installdir/bin/bamtools";
 			
 			my $sam_installdir	=	$self->getInstallDir("samtools");
 	        my $samtools	=	"$sam_installdir/samtools";
 				
 			#/agua/apps/bamtools/v2.3.0/bin/bamtools  filter -isMapped false -isPaired false -isProperPair false -in /data/nrc/SRR645386/SRR645386.fxmt.bam -out /data/nrc/SRR645386/SRR645386.fxmt_flt.bam
-			my $command	=	qq{bamtools filter -isMapped true -isPaired true -isProperPair true -in $inputfile -out $outputfile /
-			samtools index $outputfile};
+			my $command	=	qq{$bamtools filter -isMapped true -isPaired true -isProperPair true -in $inputfile -out $outputfile; $samtools index $outputfile};
 			
 			$self->logDebug("command", $command);
 	
+			`$command`;
 	}
 	else {
 			my $bam_installdir	=	$self->getInstallDir("bamtools");
-	        my $bamtools	=	"$bam_installdir/bamtools";
+	        my $bamtools	=	"$bam_installdir/bin/bamtools";
 			
 			my $sam_installdir	=	$self->getInstallDir("samtools");
 	        my $samtools	=	"$sam_installdir/samtools";
 
-			my $command	=	qq{bamtools filter -isMapped true -in $inputfile -out $outputfile /
-			samtools index $outputfile};
+			my $command	=	qq{$bamtools filter -isMapped true -in $inputfile -out $outputfile; $samtools index $outputfile};
 			$self->logDebug("command", $command);
 			
-		
+			`$command`;
 	}
 
 	$self->logDebug("COMPLETED");	
 }
 
 
-method bamIsAPair ($inputfile) {
+method isPairedBam ($inputfile) {
 	$self->logDebug("inputfile", $inputfile);
 	
-	my $output	=	$self->getBamLines($inputfile);
-	#$self->logDebug("output", $output);			
+	my $sam_installdir	=	$self->getInstallDir("samtools");
+	my $samtools	=	"$sam_installdir/samtools";
 
-	my $lines;
-	my $count = 0;
-	@$lines		=	split "\n", $output;
-	for (my $i=0 ; $i<@$lines ; $i++ ) {
-		my $line = $$lines[$i];
-		my ($header) = $line =~ /^(\S+)/;
-		$self->logDebug("header",$header);
-		
-		if ($i > 0) {
-			my ($previous) = $$lines[$i-1] =~ /^(\S+)/;
-		    $self->logDebug("previous",$previous);
-			
-			if ($previous eq $header) {
-			
-				my ($previous_flag) = $$lines[$i-1] =~ /^\S+\s+(\d+)/;
-				my ($flag) = $line =~ /^\S+\s+(\d+)/;
-				
-				if ($flag % 2 == 1 and $previous_flag % 2 == 1) {
-					return 1;
-				}
-				
-			}
-			
-		}
-		
-		my $tabs;
-		@$tabs		=	split " ", $line;
-		my $entry   =   $$tabs[0];
-		my $pair    =   substr($entry,-2,2);
-		
-		if ( $pair =~ '\D(1|2)'  ) {
-				return 1;
-			
-		}
-	}
+	my $command	=	qq{$samtools flagstats $inputfile};
+	$self->logDebug("command", $command);
+	
+	my $stats = `$command`;
+	$self->logDebug("stats", $stats);
+	
+	return 1 if $stats =~ /paired in sequencing/ms;
 	
 	return 0;	
 }
